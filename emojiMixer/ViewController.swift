cb647d7a2a5abb8e65fb91661fc87c6e6cbf6145
Sergey Kemenov <skemenov@icloud.com>
2023-10-04 23:30:29 +0300

    Made initial `EmojiMixer` app: Removed storyboard Made collectionView, custom `EmojiCell` Conformed ViewController onto `UICollectionViewDelegateFlowLayout`, `UICollectionViewDataSource` protocols

--- emojiMixer/ViewController.swift
+++ emojiMixer/ViewController.swift
@@ -7,13 +7,104 @@
 
 import UIKit
 
+// MARK: - Class ViewController
+
 class ViewController: UIViewController {
+  // MARK: - Private properties
+
+  private var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+
+  private let emojis = [
+    "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍒", "🍓", "🫐", "🥝",
+    "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🫑", "🥒", "🥬", "🥦", "🧄", "🧅", "🍄"
+  ]
+
+  // MARK: - Inits
+
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Lifecircle
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .white
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.register(EmojiCell.self, forCellWithReuseIdentifier: "cell")
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    view.addSubview(collectionView)
+
+    NSLayoutConstraint.activate([
+      collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+      collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+    ])
   }
-
-
 }
 
+// MARK: - UICollectionViewDataSource
+
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return emojis.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? EmojiCell else { return UICollectionViewCell() }
+    cell.titleEmoji.text = emojis[indexPath.row]
+    return cell
+  }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = collectionView.bounds.width / 2 - 5
+    return CGSize(width: width, height: width / 2)
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return 2
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 2
+  }
+}
+
+// MARK: - Class EmojiCell
+
+class EmojiCell: UICollectionViewCell {
+  // MARK: - Public properties
+
+  let titleEmoji: UILabel = {
+    $0.font = UIFont.systemFont(ofSize: 36)
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    return $0
+  }(UILabel(frame: .zero))
+
+  // MARK: - Inits
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    contentView.addSubview(titleEmoji)
+
+    NSLayoutConstraint.activate([
+      titleEmoji.centerXAnchor.constraint(equalTo: centerXAnchor),
+      titleEmoji.topAnchor.constraint(equalTo: topAnchor),
+      titleEmoji.bottomAnchor.constraint(equalTo: bottomAnchor)
+    ])
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
